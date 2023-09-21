9a3c780e6f90a875d0bf5b9f62e84e6350b57dce
zhangatle <zhangatle@gmail.com>
2023-09-21 15:38:57 +0800

    tencent_health

--- script/tencent-health-request-rewrite.js
+++ script/tencent-health-request-rewrite.js
@@ -17,5 +17,39 @@
 // You can optional change the response headers at the same time by using $done({body: modifiedBody, headers: modifiedHeaders}); only change the response headers is not allowed for script-response-body. The modifiedHeaders can be copied and modified from $response.headers, please do not change the content length, type and encoding field.
 // Response status can also be optional changed by using $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus}), the modifiedStatus should be like "HTTP/1.1 200 OK"
 
-$notify("腾讯健康", "session", $request.headers["sessionid"]);
+let token = $request.headers["sessionid"]
+$notify("腾讯健康", "session", token);
+// 更新青龙的环境变量
+(async function() {
+    await refreshToken(token);
+})();
+
 $done();
+
+
+function refreshToken(token) {
+    const pointUrl = { //查询积分接口
+        url: 'http://192.168.10.99:5700/open/envs',
+        headers: { //请求头
+            'Authorization': "Bearer 02a953bc-916e-4b1e-b8e5-5298be7789ca"
+        },
+        body: JSON.stringify(
+            [{"name":"PC_TOKEN", "value":token, "remarks":"test"}]
+        )
+    }
+    return new Promise((resolve) => {
+        $.post(pointUrl, (error, resp, data) => {
+            try {
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log(data)
+                }
+            } catch (e) { //接住try代码块中抛出的异常, 并打印日志
+                console.log("error");
+            } finally {
+                resolve();
+            }
+        })
+    })
+}
