bd8540f2e4748591abb2e49a0ba41877269835ea
zhangatle <zhangatle@gmail.com>
2023-09-21 16:42:32 +0800

    debug

--- script/tencent-health-request-rewrite.js
+++ script/tencent-health-request-rewrite.js
@@ -1,22 +1,3 @@
-/**
- * @fileoverview Example of HTTP rewrite.
- *
- * @supported Quantumult X (v1.0.5-build173)
- */
-
-// $request, $response, $notify(title, subtitle, message), console.log(message)
-// $request.scheme, $request.method, $request.url, $request.path, $request.headers
-// $response.statusCode, $response.headers, $response.body
-//
-// $prefs is for persistent store and the data of $prefs will be cleared when Quantumult X is deleted.
-// $prefs.setValueForKey(value, key), $prefs.removeValueForKey(key), $prefs.removeAllValues(). Returns true or false, value and key should be string.
-// $prefs.valueForKey(key) returns value.
-//
-// setTimeout(function() { console.log("abc"); }, 1000);
-//
-// You can optional change the response headers at the same time by using $done({body: modifiedBody, headers: modifiedHeaders}); only change the response headers is not allowed for script-response-body. The modifiedHeaders can be copied and modified from $response.headers, please do not change the content length, type and encoding field.
-// Response status can also be optional changed by using $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus}), the modifiedStatus should be like "HTTP/1.1 200 OK"
-
 let token = $request.headers["sessionid"]
 $notify("腾讯健康", "session", token);
 // 更新青龙的环境变量
@@ -28,6 +9,9 @@ $done();
 function refreshToken(token) {
     const myRequest = {
         url: 'http://192.168.10.99:5700/open/envs',
+        opts: {
+            policy: "direct"
+        },
         method: "POST",
         headers: { //请求头
             'Authorization': "Bearer 02a953bc-916e-4b1e-b8e5-5298be7789ca"
