81deef1f3bbc10e0a4161af72e73c8f47835676f
zhangatle <zhangatle@gmail.com>
2023-09-21 17:50:40 +0800

    debug

--- script/tencent-health-request-rewrite.js
+++ script/tencent-health-request-rewrite.js
@@ -1,5 +1,7 @@
 let token = $request.headers["sessionid"]
 
+$prefs.setValueForKey(token, "tencent-token")
+
 $notify("腾讯健康", "session", token);
 
 $done();
--- script/tencent-health-task.js
+++ script/tencent-health-task.js
@@ -1,24 +1,28 @@
 
-const options = {
-    url: 'http://192.168.10.99:5700/open/envs',
-    opts: {
-        policy: "direct"
-    },
-    method: "POST",
-    headers: { //请求头
-        'Authorization': "Bearer 02a953bc-916e-4b1e-b8e5-5298be7789ca",
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify([{"name":"abc", "value":"ssss", "remarks":"腾讯健康token"}])
-};
+let token = $prefs.valueForKey("tencent-token")
+
+if(token) {
+    const options = {
+        url: 'http://192.168.10.99:5700/open/envs',
+        opts: {
+            policy: "direct"
+        },
+        method: "POST",
+        headers: { //请求头
+            'Authorization': "Bearer 02a953bc-916e-4b1e-b8e5-5298be7789ca",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify([{"name":"tencent-health", "value": token, "remarks":"腾讯健康token"}])
+    };
 
 
-$task.fetch(options).then(response => {
-    console.log(response.body);
-    $notify("Title", "Subtitle", response.body); // Success!
-    $done();
-}, reason => {
-    console.log(reason.error)
-    $notify("Title", "Subtitle", reason.error); // Error!
-    $done();
-});
+    $task.fetch(options).then(response => {
+        console.log(response.body);
+        $notify("Title", "Subtitle", response.body); // Success!
+        $done();
+    }, reason => {
+        console.log(reason.error)
+        $notify("Title", "Subtitle", reason.error); // Error!
+        $done();
+    });
+}
